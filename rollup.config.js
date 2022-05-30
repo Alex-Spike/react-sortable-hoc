61ee64f49c93c4e8662a11897dbb363bd176133e
Alexey Nikiforov <djalexspikeby@gmail.com>
2022-05-30 17:39:47 +0300

    Add prefix

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ import pkg from './package.json';
 
 const external = (id) => !id.startsWith('.') && !id.startsWith('/');
 
-const getPath = (path) => `_${path}`;
+const getPath = (path) => `src_${path}`;
 
 const babelConfig = (
   {useESModules, targets} = {
